ba6d25c83a218eebe6884346262e42c9ff55b57f
Nattan <nattan078@gmail.com>
2022-03-30 08:57:34 -0300

    Número 2

---
+++ Alg-02/NMT-Alg-02-Ex-02.py
@@ -0,0 +1,12 @@
+segundos = int(input("Informe a quantidade de segundos: "))
+
+dias = segundos // (24*60*60)
+segundos = segundos - (dias * (24*60*60))
+
+horas = segundos // (60*60)
+segundos = segundos - (horas * (60*60))
+
+minutos = segundos // 60
+segundos = segundos - (minutos * 60)
+
+print("%d:%.2d:%2d:%2d" % (dias, horas, minutos, segundos))
